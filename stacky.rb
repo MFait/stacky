730c14fd7625cc64407d3fda1bd6f2ac64f9c087
MFait <mfait@thoughtworks.com>
2013-07-04 07:52:09 +0530

    improved spike

--- stacky.rb
+++ stacky.rb
@@ -13,9 +13,20 @@ links = doc.xpath("//*[contains(@class, 'result-link')]/span/a")
 # load question
 uri = 'http://stackoverflow.com/' + links.first["href"]
 doc = Nokogiri::HTML(open(uri))
+
+# out
+title = doc.xpath("//h1/a").first.text
+question = doc.xpath("//div[contains(@class,'post-text')]").first.text
 answer = doc.xpath("//*[contains(@class, 'vote-accepted-on')]/../../..//div[@class='post-text']").first.text
 
+puts "----------------------------"
+puts title
+puts "----------------------------"
+puts question
+puts "----------------------------"
 puts answer
+puts "----------------------------"
+
 
 
 
